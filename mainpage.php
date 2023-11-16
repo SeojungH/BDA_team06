1458d4f5c52cb8da285ab39d5d2fe50255dbd865
chaemin lim <87525734+cmlim0070@users.noreply.github.com>
2023-11-16 19:10:11 +0900

    Update mainpage.php

--- mainpage.php
+++ mainpage.php
@@ -148,11 +148,13 @@
           >
             Reposts
           </p>
+          <div onclick = "location.href='Mypage.php'">
           <p
             class="w-[95.44px] h-[28.5px] absolute left-[876.9px] top-[22.8px] text-[22px] font-light text-left text-[#a0a0a0]"
           >
-          <div onclick = "location.href='Mypage.php'">Mypage</div>
+          Mypage
           </p>
+          </div>
           <p
             class="w-[181.34px] h-[28.5px] absolute left-[1091.65px] top-[22.8px] text-[22px] font-light text-left text-[#a0a0a0]"
           >
